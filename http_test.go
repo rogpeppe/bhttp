cf026cec2d38e381d9a87241d7a4ebb3902b383f
Roger Peppe <rogpeppe@gmail.com>
2016-11-28 12:30:18 +0000

    update to latest bakery version

--- http_test.go
+++ http_test.go
@@ -10,13 +10,17 @@ import (
 	"net/url"
 	"strings"
 	"testing"
+	"time"
 
 	jc "github.com/juju/testing/checkers"
+	"golang.org/x/net/context"
 	gc "gopkg.in/check.v1"
-	"gopkg.in/macaroon-bakery.v1/bakery"
-	"gopkg.in/macaroon-bakery.v1/bakery/checkers"
-	"gopkg.in/macaroon-bakery.v1/bakerytest"
-	"gopkg.in/macaroon-bakery.v1/httpbakery"
+	"gopkg.in/errgo.v1"
+	"gopkg.in/macaroon-bakery.v2-unstable/bakery"
+	"gopkg.in/macaroon-bakery.v2-unstable/bakery/checkers"
+	"gopkg.in/macaroon-bakery.v2-unstable/bakerytest"
+	"gopkg.in/macaroon-bakery.v2-unstable/httpbakery"
+	"gopkg.in/macaroon.v2-unstable"
 	flag "launchpad.net/gnuflag"
 )
 
@@ -96,10 +100,10 @@ func (*suite) TestParseArg(c *gc.C) {
 	}
 }
 
-var newContextTests = []struct {
+var newRequestTests = []struct {
 	about         string
 	args          []string
-	expectContext context
+	expectRequest request
 	expectError   string
 }{{
 	about:       "no arguments",
@@ -111,7 +115,7 @@ var newContextTests = []struct {
 }, {
 	about: "url only",
 	args:  []string{"http://foo.com/"},
-	expectContext: context{
+	expectRequest: request{
 		method: "GET",
 		url: &url.URL{
 			Scheme: "http",
@@ -122,7 +126,7 @@ var newContextTests = []struct {
 }, {
 	about: "url with get method",
 	args:  []string{"get", "http://foo.com/"},
-	expectContext: context{
+	expectRequest: request{
 		method: "GET",
 		url: &url.URL{
 			Scheme: "http",
@@ -133,7 +137,7 @@ var newContextTests = []struct {
 }, {
 	about: "method with lower case",
 	args:  []string{"GeT", "http://foo.com/"},
-	expectContext: context{
+	expectRequest: request{
 		method: "GET",
 		url: &url.URL{
 			Scheme: "http",
@@ -144,7 +148,7 @@ var newContextTests = []struct {
 }, {
 	about: "put method",
 	args:  []string{"put", "http://foo.com/"},
-	expectContext: context{
+	expectRequest: request{
 		method: "PUT",
 		url: &url.URL{
 			Scheme: "http",
@@ -155,7 +159,7 @@ var newContextTests = []struct {
 }, {
 	about: "localhost default with port",
 	args:  []string{":8080/foo"},
-	expectContext: context{
+	expectRequest: request{
 		method: "GET",
 		url: &url.URL{
 			Scheme: "http",
@@ -166,7 +170,7 @@ var newContextTests = []struct {
 }, {
 	about: "localhost default without port",
 	args:  []string{":/foo"},
-	expectContext: context{
+	expectRequest: request{
 		method: "GET",
 		url: &url.URL{
 			Scheme: "http",
@@ -177,7 +181,7 @@ var newContextTests = []struct {
 }, {
 	about: "localhost default with non-numeric port",
 	args:  []string{":foo"},
-	expectContext: context{
+	expectRequest: request{
 		method: "GET",
 		url: &url.URL{
 			Scheme: "http",
@@ -188,7 +192,7 @@ var newContextTests = []struct {
 }, {
 	about: "host name without scheme",
 	args:  []string{"foo.com"},
-	expectContext: context{
+	expectRequest: request{
 		method: "GET",
 		url: &url.URL{
 			Scheme: "http",
@@ -211,7 +215,7 @@ var newContextTests = []struct {
 		"j3=",
 		"j4:=[1,2,3]",
 	},
-	expectContext: context{
+	expectRequest: request{
 		method: "POST",
 		header: http.Header{
 			"H1":           {"hval1"},
@@ -243,7 +247,7 @@ var newContextTests = []struct {
 		"Content-Type:application/foobar",
 		"j1=123",
 	},
-	expectContext: context{
+	expectRequest: request{
 		method: "POST",
 		header: http.Header{
 			"H1":           {"hval1"},
@@ -266,7 +270,7 @@ var newContextTests = []struct {
 		"j2=",
 		"j2=another",
 	},
-	expectContext: context{
+	expectRequest: request{
 		method: "POST",
 		form: url.Values{
 			"j1": {"123"},
@@ -285,7 +289,7 @@ var newContextTests = []struct {
 		"foo.com",
 		"j1=123",
 	},
-	expectContext: context{
+	expectRequest: request{
 		method: "PUT",
 		form: url.Values{
 			"j1": {"123"},
@@ -301,7 +305,7 @@ var newContextTests = []struct {
 		"--auth", "username:password",
 		"foo.com",
 	},
-	expectContext: context{
+	expectRequest: request{
 		method: "GET",
 		url: &url.URL{
 			Scheme: "http",
@@ -319,43 +323,43 @@ func rawMessage(s string) *json.RawMessage {
 }
 
 func (*suite) TestNewContext(c *gc.C) {
-	for i, test := range newContextTests {
+	for i, test := range newRequestTests {
 		c.Logf("test %d: %s", i, test.about)
 		fset := flag.NewFlagSet("http", flag.ContinueOnError)
-		ctxt, _, err := newContext(fset, test.args)
+		req, _, err := newRequest(fset, test.args)
 		if test.expectError != "" {
 			c.Assert(err, gc.ErrorMatches, test.expectError)
 			continue
 		}
 		c.Assert(err, gc.IsNil)
-		if len(ctxt.header) == 0 {
-			ctxt.header = nil
+		if len(req.header) == 0 {
+			req.header = nil
 		}
-		if len(ctxt.urlValues) == 0 {
-			ctxt.urlValues = nil
+		if len(req.urlValues) == 0 {
+			req.urlValues = nil
 		}
-		if len(ctxt.form) == 0 {
-			ctxt.form = nil
+		if len(req.form) == 0 {
+			req.form = nil
 		}
-		if len(ctxt.jsonObj) == 0 {
-			ctxt.jsonObj = nil
+		if len(req.jsonObj) == 0 {
+			req.jsonObj = nil
 		}
-		c.Logf("url %s", ctxt.url)
-		c.Assert(ctxt, jc.DeepEquals, &test.expectContext)
+		c.Logf("url %s", req.url)
+		c.Assert(req, jc.DeepEquals, &test.expectRequest)
 	}
 }
 
-var doRequestTests = []struct {
+var requestDoTests = []struct {
 	about             string
 	url               string
-	ctxt              context
+	req               request
 	expectRequest     http.Request
 	expectRequestBody string
 	stdin             string
 }{{
 	about: "get request with header",
 	url:   "/foo",
-	ctxt: context{
+	req: request{
 		method: "GET",
 		header: http.Header{
 			"X-Something": {"foo"},
@@ -373,7 +377,7 @@ var doRequestTests = []struct {
 }, {
 	about: "get request with url values",
 	url:   "/foo",
-	ctxt: context{
+	req: request{
 		method: "GET",
 		urlValues: url.Values{
 			"x": {"xval1", "xval2"},
@@ -394,7 +398,7 @@ var doRequestTests = []struct {
 }, {
 	about: "get request with url values",
 	url:   "/foo",
-	ctxt: context{
+	req: request{
 		method: "GET",
 		urlValues: url.Values{
 			"x": {"xval1", "xval2"},
@@ -415,7 +419,7 @@ var doRequestTests = []struct {
 }, {
 	about: "get request with url values, some explicitly set",
 	url:   "/foo?z=zval",
-	ctxt: context{
+	req: request{
 		method: "GET",
 		urlValues: url.Values{
 			"x": {"xval1", "xval2"},
@@ -437,7 +441,7 @@ var doRequestTests = []struct {
 }, {
 	about: "post request with form values in body",
 	url:   "/foo",
-	ctxt: context{
+	req: request{
 		method: "POST",
 		form: url.Values{
 			"x": {"xval1", "xval2"},
@@ -457,7 +461,7 @@ var doRequestTests = []struct {
 }, {
 	about: "post request with JSON body",
 	url:   "/foo",
-	ctxt: context{
+	req: request{
 		method: "POST",
 		jsonObj: map[string]interface{}{
 			"x": "hello",
@@ -478,19 +482,19 @@ var doRequestTests = []struct {
 	expectRequestBody: `{"x":"hello"}`,
 }}
 
-func (*suite) TestDoRequest(c *gc.C) {
+func (*suite) TestRequestDo(c *gc.C) {
 	var h handler
 	srv := httptest.NewServer(&h)
-	for i, test := range doRequestTests {
+	for i, test := range requestDoTests {
 		c.Logf("test %d: %s", i, test.about)
 		client := httpbakery.NewClient()
 		u, err := url.Parse(srv.URL + test.url)
 		c.Assert(err, gc.IsNil)
-		test.ctxt.url = u
-		if test.ctxt.header == nil {
-			test.ctxt.header = make(http.Header)
+		test.req.url = u
+		if test.req.header == nil {
+			test.req.header = make(http.Header)
 		}
-		resp, err := test.ctxt.doRequest(client, strings.NewReader(test.stdin))
+		resp, err := test.req.do(client, strings.NewReader(test.stdin))
 		c.Assert(err, gc.IsNil)
 		c.Assert(resp.StatusCode, gc.Equals, http.StatusOK)
 		resp.Body.Close()
@@ -511,45 +515,55 @@ func (*suite) TestDoRequest(c *gc.C) {
 
 func (*suite) TestMacaraq(c *gc.C) {
 	checked := false
-	d := bakerytest.NewDischarger(nil, func(_ *http.Request, cond, arg string) ([]checkers.Caveat, error) {
-		if cond != "something" {
+	d := bakerytest.NewDischarger(nil, httpbakery.ThirdPartyCaveatCheckerFunc(func(_ *http.Request, info *bakery.ThirdPartyCaveatInfo) ([]checkers.Caveat, error) {
+		if info.Condition != "something" {
 			return nil, fmt.Errorf("unexpected 3rd party cond")
 		}
 		checked = true
-		return nil, nil
-	})
+		return []checkers.Caveat{
+			checkers.DeclaredCaveat("username", "bob"),
+		}, nil
+	}))
 
-	bsvc, err := bakery.NewService(bakery.NewServiceParams{
-		Location: "here",
-		Locator:  d,
-	})
+	key, err := bakery.GenerateKey()
 	c.Assert(err, gc.IsNil)
+	b := bakery.New(bakery.BakeryParams{
+		Location:       "here",
+		Locator:        d,
+		Key:            key,
+		IdentityClient: idmClient{d.Location()},
+	})
 	svc := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		req.ParseForm()
-		_, checkErr := httpbakery.CheckRequest(bsvc, req, nil, checkers.New())
+
+		authInfo, checkErr := b.Checker.Auth(httpbakery.RequestMacaroons(req)...).Allow(context.TODO(), bakery.LoginOp)
 		if checkErr == nil {
+			c.Check(authInfo.Identity.Id(), gc.Equals, "bob")
 			w.Header().Set("Content-Type", "application/json")
 			data, err := json.Marshal(req.Form)
 			c.Check(err, gc.IsNil)
 			w.Write(data)
 			return
 		}
-		m, err := bsvc.NewMacaroon("", nil, []checkers.Caveat{{
-			Location:  d.Service.Location(),
-			Condition: "something",
-		}})
+		derr, ok := errgo.Cause(checkErr).(*bakery.DischargeRequiredError)
+		if !ok {
+			c.Error("got non-discharge-required error: %v", checkErr)
+			http.Error(w, "unexpected error", http.StatusInternalServerError)
+			return
+		}
+		m, err := b.Oven.NewMacaroon(context.TODO(), macaroon.LatestVersion, time.Now().Add(time.Hour), derr.Caveats, derr.Ops...)
 		c.Check(err, gc.IsNil)
 		httpbakery.WriteDischargeRequiredError(w, m, "/", checkErr)
 	}))
 
 	fset := flag.NewFlagSet("http", flag.ContinueOnError)
-	ctxt, params, err := newContext(fset, []string{
+	req, params, err := newRequest(fset, []string{
 		svc.URL,
 		"x=y",
 	})
 	c.Assert(err, gc.IsNil)
 	client := httpbakery.NewClient()
-	resp, err := ctxt.doRequest(client, nil)
+	resp, err := req.do(client, nil)
 	c.Assert(err, gc.IsNil)
 	defer resp.Body.Close()
 	c.Assert(resp.StatusCode, gc.Equals, http.StatusOK)
@@ -585,4 +599,29 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	if h.next != nil {
 		h.next.ServeHTTP(w, req)
 	}
+}
+
+type idmClient struct {
+	dischargerURL string
+}
+
+func (c idmClient) IdentityFromContext(ctxt context.Context) (bakery.Identity, []checkers.Caveat, error) {
+	return nil, []checkers.Caveat{{
+		Location:  c.dischargerURL,
+		Condition: "something",
+	}}, nil
+}
+
+func (c idmClient) DeclaredIdentity(declared map[string]string) (bakery.Identity, error) {
+	return simpleIdentity(declared["username"]), nil
+}
+
+type simpleIdentity string
+
+func (simpleIdentity) Domain() string {
+	return ""
+}
+
+func (id simpleIdentity) Id() string {
+	return string(id)
 }
